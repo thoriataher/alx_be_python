6a31d451645e1fbee6a74e20e6968e8ac182e60b
thoriataher <thoriataher08@gmail.com>
2024-09-15 03:35:14 +0200

    change monthly_income and monthly_saving format to float

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,6 @@
 monthly_income = float(input("Enter your monthly income: "))
-total_monthly_expenses = float(input("Enter your total monthly expenses: "))
-
-monthly_savings = monthly_income - total_monthly_expenses
-
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 print(f"Your monthly savings are ${monthly_savings:.2f}.")
