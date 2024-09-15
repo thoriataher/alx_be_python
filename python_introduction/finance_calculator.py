78610745e33071a843f13b54307e1ccc71b0f0a5
thoriataher <thoriataher08@gmail.com>
2024-09-15 03:28:58 +0200

    change projected saving format to integer

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,7 +3,7 @@ total_monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 monthly_savings = abs(total_monthly_expenses - monthly_income)
 
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest, is: ${projected_savings}")
